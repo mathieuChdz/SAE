a0bebe5acf6481dc34a66755437bca07069ccf86
Ludovic <danlos.ludo@gmail.com>
2022-12-14 21:35:34 +0100

    Ajustement de la syntaxe pour une meilleure lisibilité

--- src/Codes_maquette1/module_crypto/CryptoRC4.py
+++ src/Codes_maquette1/module_crypto/CryptoRC4.py
@@ -1,42 +1,45 @@
-def ascii(message : str) -> list:
+def ascii(message: str) -> list:
     """Transforme une chaine de caracteres en un tableau contenant les codes ascii correspondant a chauqe caractere.
     Entree :
         message (str) -> un mot ou une phrase sous forme de chaine de caracteres.
     Sortie :
         tab (list) -> un tableau contenant les codes ascii de chaque caractere du message entre en parametre."""
     
-    tab=[]
+    tab = []
     #On parcourt le message caractere par caractere et 
     #on stocke dans un tableau le code ascii correspondant au caractere qu'on regarde
     for i in message:
         tab.append(ord(i))
-    return(tab)
-ascii('Secret')
+    return tab
+
+
+print(ascii('Secret'))
 
 
 
-def perm(key : str) -> list:
+def perm(key: str) -> list:
     """Genere une chaine sous forme de tableau a partir d'une permutation d'une clef entree en parametre.
     Entree :
         key (str) ->  une clef sous forme de chaine de caracteres, cela peut etre un mot ou une phrase.
     Sortie :
         S (list) -> un tableau compose de codes ascii (des entiers) issus d'une permutation de la clef."""
     
-    S=list(range(256))  
-    key=ascii(key)
+    S = list(range(256))
+    key = ascii(key)
     
-    j=0
+    j = 0
     for i in range(256):
        
-        j=(j+S[i]+key[i%len(key)])%256
-        S[i],S[j]=S[j],S[i]
+        j = (j + S[i] + key[i % len(key)]) % 256
+        S[i], S[j] = S[j], S[i]
         
-    return(S)
+    return S
     
+
 print(perm('Secret'))
 
 
-def codage(key : str, message : str) -> list:
+def codage(key: str, message: str) -> list:
     """Chiffre un message a l'aide d'une clef en utilisant le principe du chiffrement RC4.
     Entrees :
         key (str) -> une clef sous forme de chaine de caracteres, cela peut etre un mot ou une phrase.
@@ -47,9 +50,9 @@ def codage(key : str, message : str) -> list:
     i = 0
     j = 0
     S = perm(key)
-    code=[None]*len(message)
-    key=ascii(key)
-    message=ascii(message)
+    code = [None] * len(message)
+    key = ascii(key)
+    message = ascii(message)
     
     
     for n in range(len(message)):
@@ -57,14 +60,16 @@ def codage(key : str, message : str) -> list:
         i = (i+1) % 256
         j = (j + S[i]) % 256
 
-        S[i],S[j]=S[j],S[i]
-        code[n]=f'{S[(S[i] + S[j]) % 256 ] ^(message[n]):x}'
+        S[i], S[j] = S[j], S[i]
+        code[n] = f'{S[(S[i] + S[j]) % 256 ] ^(message[n]):x}'
     
     return code
-print(codage('Secret','Attack at dawn'))
 
 
-def decodage(key : str, message : list) -> str:
+print(codage('Secret', 'Attack at dawn'))
+
+
+def decodage(key: str, message: list) -> str:
     """Dechiffre un message chiffre avec le chiffrement RC4 a l'aide de la meme clef qui a ete utilise pour le chiffrement.
     Entrees :
         key (str) -> la clef utilisee pour chiffrer le message sous forme de chaine de caracteres,
@@ -77,15 +82,17 @@ def decodage(key : str, message : list) -> str:
     i = 0
     j = 0
     S = perm(key)
-    code=[None]*len(message)
-    key=ascii(key)
+    code = [None] * len(message)
+    key = ascii(key)
     
     for n in range(len(message)):
-        i = (i+1) % 256
+        i = (i + 1) % 256
         j = (j + S[i]) % 256
 
-        S[i],S[j]=S[j],S[i]
-        code[n]=chr(S[(S[i] + S[j]) % 256 ] ^int(message[n], base=16))
+        S[i], S[j] = S[j], S[i]
+        code[n] = chr(S[(S[i] + S[j]) % 256] ^ int(message[n], base=16))
     
     return ''.join(code)
-print(decodage('Secret',codage('Secret','Attack at dawn')))
+
+
+print(decodage('Secret', codage('Secret', 'Attack at dawn')))
