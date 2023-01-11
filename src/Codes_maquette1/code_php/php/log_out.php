557a0c813d511b6c2f0c3b838989837159936d92
unknown <chedozeaumathieu@gmail.com>
2023-01-11 19:52:26 +0100

    commentaires de la page log out

--- src/Codes_maquette1/code_php/php/log_out.php
+++ src/Codes_maquette1/code_php/php/log_out.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
+// vérifie que l'utilisateur est connecté
 if (isset($_SESSION['login'], $_SESSION['admin'])){
+    
+    // vide les variables 'login' et 'admin'
     unset($_SESSION['login']);
     unset($_SESSION['admin']);
     header("Location: ../index.php");
