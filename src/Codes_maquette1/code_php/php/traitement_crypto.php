d49495a9397c28b4cd775c00bb54f19083f44beb
unknown <chedozeaumathieu@gmail.com>
2023-01-11 18:19:25 +0100

    commentaire de la page des traitements du module de chiffrement

--- src/Codes_maquette1/code_php/php/traitement_crypto.php
+++ src/Codes_maquette1/code_php/php/traitement_crypto.php
@@ -4,42 +4,73 @@ session_start();
 
 if (isset($_POST['message'])){
     
+    // connexion à la base de donnée
     $connexion=mysqli_connect("localhost", "root", "01r1173");
     $bd=mysqli_select_db($connexion, "Utilisateurs");
 
+    // partie chiffrement
     if (isset($_POST['chiffrer'])){
+
+        // vérification que les variables 'chiffrer' et 'message' ne sont pas nulles
         if ($_POST['chiffrer']!=null and $_POST['message']!=null){
-            $arg1 = $_POST['message'];
-            $arg2 = $_POST['cle'];
 
-            $resultat = exec("python ../python/codage.py $arg1 $arg2"); 
-
-            $ins_chiffrer = "INSERT into crypto_historique(login,action,message,resultat) values(?,?,?,?)";
-            $insp_chiffrer = mysqli_prepare($connexion, $ins_chiffrer);
-            $login = $_SESSION['login'];
-            $action = "chiffrer";
-            mysqli_stmt_bind_param($insp_chiffrer, 'ssss', $login, $action, $arg1, $resultat);
-            mysqli_stmt_execute($insp_chiffrer);
+            // vérification que la variable 'cle' n'est pas nulle
+            if ($_POST['cle']!=null){
+                $arg1 = $_POST['message'];
+                $arg2 = $_POST['cle'];
+    
+                $resultat = exec("python ../python/codage.py $arg1 $arg2"); 
+    
+                $ins_chiffrer = "INSERT into crypto_historique(login,action,message,resultat) values(?,?,?,?)";
+                $insp_chiffrer = mysqli_prepare($connexion, $ins_chiffrer);
+                $login = $_SESSION['login'];
+                $action = "chiffrer";
+                mysqli_stmt_bind_param($insp_chiffrer, 'ssss', $login, $action, $arg1, $resultat);
+                mysqli_stmt_execute($insp_chiffrer);
+            }
+            // la variable clé est nulle
+            else{
+                header("Location: simu_crypto.php?err=1");
+            }
+        }
+        // la variables message est nulle
+        else{
+            header("Location: simu_crypto.php?err=2");
         }
     }
 
+    // partie déchiffrement
     if (isset($_POST['dechiffrer'])){
-        if ($_POST['dechiffrer']!=null and $_POST['message']!=null){
-            $arg1 = $_POST['message'];
-            $arg2 = $_POST['cle'];
 
-            $resultat = exec("python ../python/decodage.py $arg1 $arg2"); 
-            
-            $ins_dechiffrer = "INSERT into crypto_historique(login,action,message,resultat) values(?,?,?,?)";
-            $insp_dechiffrer = mysqli_prepare($connexion, $ins_dechiffrer);
-            $login = $_SESSION['login'];
-            $action = "déchiffrer";
-            mysqli_stmt_bind_param($insp_dechiffrer, 'ssss', $login, $action, $arg1, $resultat);
-            mysqli_stmt_execute($insp_dechiffrer);
+        // vérification que les variables 'chiffrer' et 'message' ne sont pas nulles
+        if ($_POST['dechiffrer']!=null and $_POST['message']!=null){
+
+            // vérification que la variable 'cle' n'est pas nulle
+            if ($_POST['cle']!=null){
+                $arg1 = $_POST['message'];
+                $arg2 = $_POST['cle'];
+
+                $resultat = exec("python ../python/decodage.py $arg1 $arg2"); 
+                
+                $ins_dechiffrer = "INSERT into crypto_historique(login,action,message,resultat) values(?,?,?,?)";
+                $insp_dechiffrer = mysqli_prepare($connexion, $ins_dechiffrer);
+                $login = $_SESSION['login'];
+                $action = "déchiffrer";
+                mysqli_stmt_bind_param($insp_dechiffrer, 'ssss', $login, $action, $arg1, $resultat);
+                mysqli_stmt_execute($insp_dechiffrer);
+            }
+            // la variable clé est nulle
+            else{
+                header("Location: simu_crypto.php?err=1");
+            }
+        }
+        // la variables message est nulle
+        else{
+            header("Location: simu_crypto.php?err=2");
         }
     }
 
-
+    // Incrémente l'attribut 'module_2' de l'utilisateur dans la tables des statistiques
     $select="SELECT module_2 FROM users_stats WHERE login='".$_SESSION['login']."'";
     $res=mysqli_query($connexion, $select);
 
@@ -51,9 +82,5 @@ if (isset($_POST['message'])){
     mysqli_stmt_bind_param($insp2, 'i', $nb);
 	mysqli_stmt_execute($insp2);
 }
-
-
-header("Location: simu_crypto.php?res=$resultat");
-
 
 ?>
